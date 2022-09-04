505aba973f88dcab9c480b7b98a9052530173543
Oleg Adamov <olegadamov84@gmail.com>
2022-09-04 23:03:55 +0300

    04.09-23.01

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-function fetchCountries() {
-    return fetch(`${BASE_URL}/all?fields=name.official,capital,population,flags,languages`)
+fetchCountries = nameCountry => {
+    return fetch(`${BASE_URL}/name/${nameCountry}?fields=name,capital,population,flags,languages`)
         .then(response => {
             return response.json();
         });
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import './css/styles.css';
-import {fetchCountries} from './fetchCountries';
+import API from './fetchCountries';
 
 const countryInput = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list')
 const countryInfo = document.querySelector('.country-info')
 var debounce = require('lodash.debounce');
 
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+// import Notiflix from 'notiflix'
 const DEBOUNCE_DELAY = 300;
 
 countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
@@ -24,21 +23,42 @@ function onSearch(event) {
     const searchCountry = form.value.trim();
     console.log('searchCountry', searchCountry)
 
-fetchCountries(searchCountry)
+API.fetchCountries(searchCountry)
     .then(renderCountry)
     .catch(onFetchError)
-    .finally(() => form.reset());
+    // .finally(() => form.reset());
 
 }
 
 function renderCountry(country) {
     const markup = countryList(country);
-    countryInput.innerHTML.countryList = markup
+    countryInput.innerHTML.countryList = markup;
+    console.log('countryInput', countryInput)
 }
 
-    function onFetchError(error) {
-alert('Упс, что-т пошло не так!')
-    }
+function onFetchError(error) {
+    // resetMarkup(reset.markup)
+    return Notiflix.Notify.failure('Oops, there is no country with that name')
+
+}
     
+
   
-console.log('Promise:', fetchCountries());
+// function markup() {
+//     fetchCountries.map(({ name, capital, population, flags, languages }) =>
+//         `<h1><img src="${flags.svg}" alt="${name.official}" width="60" height="60">${name.official}</h1>
+// <p>Capital: ${capital}</p>
+//       <p>Population: ${population}</p>
+//       <p>Languages: ${Object.values(languages)}</p>`,
+//     )
+//         // .join("");
+
+// console.log('markup', markup)
+// countryInfo.innerHTML = markup;
+// }
+
+
+
+
+
+// console.log('Promise:', API.fetchCountries());
