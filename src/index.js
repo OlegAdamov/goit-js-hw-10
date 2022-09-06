29c3616515f67a19a4ac0904c555e395e2cb1f16
Oleg Adamov <olegadamov84@gmail.com>
2022-09-06 12:25:11 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
-import Notiflix from 'notiflix'
+import Notiflix from 'notiflix';
 
 
 const countryInput = document.querySelector('#search-box');
-const countryList = document.querySelector('.country-list')
-const countryInfo = document.querySelector('.country-info')
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
@@ -18,18 +18,18 @@ countryInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch(event) {
     event.preventDefault();
     nameCountry = event.target.value.trim();
-    resetMarkup()
+    resetMarkup();
     fetchCountries(nameCountry)
         .then(countries => {
             if (countries.length > 10) {
                 Notiflix.Notify.info('Too many matches found. Please enter a more specific name.')
-            }
+            };
             renderCountry(countries)
         })
         .catch(error => {
-            Notiflix.Notify.failure('Oops, there is no country with that name')
-            resetMarkup(countryList)
-            resetMarkup(countryInfo)
+            Notiflix.Notify.failure('Oops, there is no country with that name');
+            resetMarkup(countryList);
+            resetMarkup(countryInfo);
         })
 };
 
@@ -43,13 +43,13 @@ countryList.innerHTML = list
     resetMarkup(countryInfo)
     const info = creatCountryInfo(countries)
 countryInfo.innerHTML = info
-    } 
-}
+    };
+};
 
 function createCountryList(countries) {
     return countries.map(({ name, flags }) =>
         `<li class = "item"><img src="${flags.svg}" alt"${name.official}" width="60" height="30" clall = item-img>  ${name.official}</li>`)
-        .join("")
+        .join("");
 };
     
   
@@ -61,10 +61,10 @@ function creatCountryInfo(countries) {
       <p>Languages: ${Object.values(languages)}</p>`,
     )
         .join("");
-}
+};
 
 
 function resetMarkup() {
     countryList.innerHTML = '';
     countryInfo.innerHTML = '';
-}
+};
